a1cd0b80e2ff3136de30e25b61130b7d92047b57
NurullohUbaydullayev <nurulloh.ubaydullayev2004@gmail.com>
2021-09-30 00:25:20 +0500

    Add blogs page and Single post page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import "./App.css";
+import "./App.scss";
+
+import { Switch, Route } from "react-router-dom";
+// import data from "./Components/Data/Data";
+
+// Pages
+import About from "./Pages/About/About";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Blog from "./Pages/Blog/Blog";
+import SingleBlog from "./Pages/SingleBlog/SingleBlog";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/" component={About} exact />
+        <Route path="/portfolio" component={Portfolio} exact />
+        <Route path="/blogs" component={Blog} exact />
+        <Route path="/blogs/:id" component={SingleBlog} exact />
+        <Route path="/contact" component={Contact} exact />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,8 +1,88 @@
 import "./Blog.scss";
 import React from "react";
+import { Link } from "react-router-dom";
+
+//images
+import Blogst from "../../Assets/Images/post-img.png";
+import Blognd from "../../Assets/Images/post-img2.png";
 
 function Blog() {
-  return <div className="blog">Blog</div>;
+  const data = [
+    {
+      id: 1,
+      title: "Usabilty testing it is one of the possible way?",
+      name: "What is usati testing - and why is it needed?",
+      img: Blogst,
+      tag: "#ux-ui",
+    },
+    {
+      id: 2,
+      title: "What is the Ux mean?",
+      name: "What is UX and why do you need it? let's sort out the place.",
+      tag: "#ux-ui",
+    },
+    {
+      id: 3,
+      title: "What is the Ux mean?",
+      name: "What is UX and why do you need it? let's sort out the place.",
+      tag: "#ux-ui",
+    },
+    {
+      id: 4,
+      title: "What is the Ux mean?",
+      name: "What is UX and why do you need it? let's sort out the place.",
+      tag: "#ux-ui",
+    },
+    {
+      id: 5,
+      title: "What is the Ux mean?",
+      name: "What is UX and  why do you need it? let's sort out the place.",
+      tag: "#ux-ui",
+    },
+    {
+      id: 6,
+      title: "Usabilty testing it is one of the possible way?",
+      name: "What is usati testing - and why is it needed?",
+      img: Blognd,
+      tag: "#ux-ui",
+    },
+  ];
+
+  return (
+    <>
+      <div className="blog-section">
+        <div className="blog__content">
+          <div className="blog__left-box">
+            <a className="blog__more-animation" href="/">
+              more
+            </a>
+            {data.slice(0, 3).map((row) => (
+              <Link className="blog__navlivk" to={"blogs/" + row.id}>
+                <li className="blog__list" key={row.id}>
+                  <h2 className="blog__title">{row.title}</h2>
+                  <p className="blog__name">{row.name}</p>
+                  <img className="blog__img" src={row.img} alt="" width="325" />
+                  <p className="blog__tag">{row.tag}</p>
+                </li>
+              </Link>
+            ))}
+          </div>
+          <div className="blog__right-box">
+            {data.slice(3, 6).map((row) => (
+              <Link className="blog__navlivk" to={"blogs/" + row.id}>
+                <li className="blog__list" key={row.id}>
+                  <h2 className="blog__title">{row.title}</h2>
+                  <p className="blog__name">{row.name}</p>
+                  <img className="blog__img" src={row.img} alt="" />
+                  <p className="blog__tag">{row.tag}</p>
+                </li>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Blog;
